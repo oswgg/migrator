7b102fcb2268fa0ef36b6b2126c73ebb2ff11524
Oswaldo Gonzalez <oswaldo.gg0520@gmail.com>
2024-12-24 01:52:17 -0600

    Init the very first command (#1)

---
+++ cmd/migrator_cmd.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var migratorCmd = &cobra.Command{
+	Use:   "migrator [command]",
+	Short: "Migrator its a simple CLI to run your migrations",
+	Long:  "Migrator its a simple CLI to run your sql files and migrate your database...",
+}
+
+func Execute() {
+	if err := migratorCmd.Execute(); err != nil {
+		fmt.Println(err)
+	}
+	os.Exit(1)
+}
