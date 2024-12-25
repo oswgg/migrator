38e5789bbc8dd5cef3c1904aceaa0ddfa3ce54f6
oswgg <oswaldo.gg0520@gmail.com>
2024-12-25 00:38:05 -0600

    feat: genera primeros archivos de configuracion para el usuario

---
+++ internal/config/templates.go
@@ -0,0 +1,26 @@
+package config
+
+const MigratorRCFile = `config_folder_path=config1/config`
+
+const ConfigFile = `development:
+	username: root
+	password: root
+	database: database
+	host: localhost
+	port: 3306
+	dialect: mysql
+production:
+	username: $USERNAME
+	password: $PASSWORD
+	database: $DATABASE
+	host: $HOST
+	port: $PORT
+	dialect: $DIALECT
+test:
+	username: $TEST_USERNAME
+	password: $TEST_PASSWORD
+	database: $TEST_DATABASE
+	host: $TEST_HOST
+	port: $TEST_PORT
+	dialect: $TEST_DIALECT
+`
