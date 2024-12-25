c565047e08e0688b2fb7d9b116a83d097b4db2d8
oswgg <oswaldo.gg0520@gmail.com>
2024-12-25 02:50:52 -0600

    can check connecitno to database

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/oswgg/migrator
 go 1.23.3
 
 require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/spf13/cobra v1.8.1
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/get_user_config.go
+++ internal/config/get_user_config.go
@@ -1,1 +1,91 @@
 package config
+
+import (
+	"github.com/oswgg/migrator/internal/database"
+	"github.com/oswgg/migrator/pkg/tools"
+	"gopkg.in/yaml.v3"
+	"path"
+)
+
+const (
+	Develop = "dev"
+	Prod    = "prod"
+	Test    = "test"
+)
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
+	Dialect  string `yaml:"dialect"`
+}
+
+type UserMigratorRCConfig struct {
+	ConfigFolderPath string
+}
+
+type UserYAMLConfig struct {
+	Development DatabaseConfig `yaml:"development"`
+	Production  DatabaseConfig `yaml:"production"`
+	Test        DatabaseConfig `yaml:"test"`
+}
+
+func GetUserTxTConfig() (*UserMigratorRCConfig, error) {
+	txtValues, err := tools.GetTxtValues(migratorRCFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserMigratorRCConfig{
+		ConfigFolderPath: txtValues["config_folder_path"],
+	}, nil
+}
+
+func GetUserYAMLConfig(env string) (*database.DatabaseCredentials, error) {
+	var err error
+	var userTxtConfigs *UserMigratorRCConfig
+
+	userTxtConfigs, err = GetUserTxTConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	userConfig := UserYAMLConfig{}
+
+	yamlContent, err := tools.ReadFile(path.Join(userTxtConfigs.ConfigFolderPath, configYamlFileName))
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(yamlContent, &userConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	switch env {
+	case Develop:
+		return &database.DatabaseCredentials{
+			Host:     userConfig.Development.Host,
+			Port:     userConfig.Development.Port,
+			Username: userConfig.Development.Username,
+			Password: userConfig.Development.Password,
+			Database: userConfig.Development.Database,
+			Dialect:  userConfig.Development.Dialect,
+		}, nil
+
+	case Prod:
+		return &database.DatabaseCredentials{
+			Host:     userConfig.Production.Host,
+			Port:     userConfig.Production.Port,
+			Username: userConfig.Production.Username,
+			Password: userConfig.Production.Password,
+			Database: userConfig.Production.Database,
+			Dialect:  userConfig.Production.Dialect,
+		}, nil
+
+	}
+
+	return nil, err
+}
